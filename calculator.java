2412a6ca1803d186acfc1369a56e5003ef5330de
Seçkin Abdullah Kara <seckinkaraa@gmail.com>
2025-02-04 06:46:13 -0500

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -2,61 +2,57 @@ import java.lang.Math;
 import java.util.*;
 
 
-class calculator {
+public class Calculator {
 
+    HashMap<String, Integer> functDict = new HashMap<String, Integer>();
+    ArrayList<String> functs = new ArrayList<String>(Arrays.asList("tan", "lg", "cos", "ln", "mod", "sin", "sqrt", "exp"));
+    ArrayList<String> singleFuncts = new ArrayList<String>(Arrays.asList("+", "-", "*", "/", "^"));
+    HashMap<String, Double> varDict = new HashMap<String, Double>();
     
+    Calculator() {
+        functDict.put("+", 1);
+        functDict.put("-", 1);
+        functDict.put("*", 2);
+        functDict.put("/", 2);
+        functDict.put("^", 3);
+        functDict.put("sqrt", 4);
+        functDict.put("exp", 4);
+        functDict.put("sin", 4);
+        functDict.put("cos", 4);
+        functDict.put("tan", 4);
+        functDict.put("ln", 4);
+        functDict.put("mod", 4);
+        functDict.put("lg", 4);
+
+    }
+
+
     private class treeNode {
 
         public String value;
-        public int lchild;
-        public int rchild;
+        public treeNode lchild;
+        public treeNode rchild;
 
-        public treeNode(String new_value, int new_lchild, int new_rchild) {
+        public treeNode(String new_value, treeNode new_lchild, treeNode new_rchild) {
             value = new_value;
             lchild = new_lchild;
             rchild = new_rchild;
         }
         public treeNode(String new_value) {
             value = new_value;
-            lchild = 0;
-            rchild = 0;
+            lchild = null;
+            rchild = null;
         }
         public treeNode() {
-            value = "";
-            lchild = 0;
-            rchild = 0;
+            value = "__return__";
+            lchild = null;
+            rchild = null;
         }
     }        
 
-    HashMap<String, Integer> functDic = new HashMap<String, Integer>();
-    List<String> functList;
-    ArrayList<String> functs = new ArrayList<String>(Arrays.asList("tan", "lg", "cos", "ln", "mod", "sin", "sqrt", "exp"));
-    ArrayList<String> singleFuncts = new ArrayList<String>(Arrays.asList("+", "-", "*", "/", "^"));
-    ArrayList<String[]> all_lines = new ArrayList<String[]>(); 
-    HashMap<String, Double> varDic = new HashMap<String, Double>();
-    ArrayList<treeNode> nodes = new ArrayList<treeNode>();
 
-    public calculator() {
-        functDic.put("+", 1);
-        functDic.put("-", 1);
-        functDic.put("*", 2);
-        functDic.put("/", 2);
-        functDic.put("^", 3);
-        functDic.put("sqrt", 4);
-        functDic.put("exp", 4);
-        functDic.put("sin", 4);
-        functDic.put("cos", 4);
-        functDic.put("tan", 4);
-        functDic.put("ln", 4);
-        functDic.put("mod", 4);
-        functDic.put("lg", 4);
-        functList = new ArrayList(functDic.keySet()); 
-        treeNode root = new treeNode("0");
-        nodes.add(root);
-
-    }
-
-    public void getLines(String expr) {
+    public ArrayList<String[]> getLines(String expr) {
+        ArrayList<String[]> all_lines = new ArrayList<String[]>();
         String[] lines = expr.split(";");
         if (lines.length == 1 && !lines[0].contains("return")) {
             lines[0] = "return " + lines[0];
@@ -67,21 +63,17 @@ class calculator {
                 each = lines[i].split("=");
                 all_lines.add(each);
             } else if (lines[i].matches("\\s*return .+")) {
-                each = lines[i].split("return");
+                each = lines[i].split("return ");
                 each[0] = "return";
                 all_lines.add(each);
             }
         }
+        return all_lines;
     }
                 
 
-    public double applyFunct(String funct, double operand1, double operand2) {
+    public double applyFunct(String funct, double operand1) {
         switch (funct) {
-            case "+": return operand1+operand2;
-            case "-": return operand1-operand2;
-            case "*": return operand1*operand2;
-            case "/": return operand1/operand2;
-            case "^": return Math.pow(operand1, operand2);
             case "sqrt": return Math.sqrt(operand1);
             case "exp": return Math.exp(operand1);
             case "sin": return Math.sin(operand1); 
@@ -89,20 +81,38 @@ class calculator {
             case "tan": return Math.tan(operand1);
             case "ln": return Math.log(operand1);
             case "lg": return Math.log(operand1)/Math.log(2);
+            }
+        return 0;
+    }
+    
+        public double applyFunct(String funct, double operand1, double operand2) {
+        switch (funct) {
+            case "+": return operand1+operand2;
+            case "-": return operand1-operand2;
+            case "*": return operand1*operand2;
+            case "/": return operand1/operand2;
+            case "^": return Math.pow(operand1, operand2);
             case "mod": return operand1 - operand2*Math.floor(operand1/operand2);
             }
         return 0;
     }
 
-
     public boolean isVariable(String expr) {
-        if (expr.trim().length() == 0) {return false;}
+        if (expr.trim().length() == 0) {
+            return false;
+        }
         expr = expr.trim();
-        if (expr.substring(0,1) == "") {return false;}
-        if (!expr.substring(0, 1).matches("[a-zA-Z]+")) {return false;}
+        if (expr.substring(0, 1) == "") {
+            return false;
+        }
+        if (!expr.substring(0, 1).matches("[a-zA-Z]+")) {
+            return false;
+        }
         else {
             for (int i = 1; i < expr.length(); i++) {
-                if (!expr.substring(i, i+1).matches("[a-zA-Z]+") && !expr.substring(i, i+1).matches("[0-9]+")) {return false;}
+                if (!expr.substring(i, i+1).matches("[0-9a-zA-Z]+")) {
+                    return false;
+                }
             } return true;
         }
     }
@@ -144,7 +154,7 @@ class calculator {
                 nestLevel -= 1;
             } else if (masked[i] == '(') {
                 nestLevel += 1;
-            } if ((nestLevel>0) && !(masked[i]=='(')) {
+            } if ((nestLevel>0) && !(Character.toString(masked[i]).matches("[()]"))) {
                 masked[i] = ' ';
             }
         } String b = new String(masked);
@@ -153,30 +163,39 @@ class calculator {
 
 
     public int getNextFunctPos(String expr, int pos) {
-        if (expr.length() == 0) {return -1;}
-        int oprPos=-1;
+        if (expr.length() == 0) {
+            return -1;
+        }
+        int oprPos = -1;
         char opr;
         while (pos < expr.length()) {
-            if (functList.contains(Character.toString(expr.charAt(pos)))) {
+            if (functDict.containsKey(Character.toString(expr.charAt(pos)))) {
                 oprPos = pos;
                 opr = expr.charAt(pos);
                 if ((opr == '-') && (pos==0)) {
-                    if ((expr.substring(pos+1).trim().charAt(0) == '(') || (Character.isLetter(expr.substring(pos+1).trim().charAt(0)))) {break;}
+                    if ((expr.substring(pos+1).trim().charAt(0) == '(') || (Character.isLetter(expr.substring(pos+1).trim().charAt(0)))) {
+                        break;
+                    }
                     else {
                         pos += 1;
                         continue;
                     }
-                } else {break;}
+                } else {
+                    break;
+                }
             } else if (Character.isLetter(expr.charAt(pos))) {
                 int scanPos = pos;
                 while (Character.isLetter(expr.charAt(scanPos))) {
                     scanPos += 1;
-                    if (scanPos >= expr.length()) {break;}
-                } if (functList.contains(expr.substring(pos, scanPos))) {
+                    if (scanPos >= expr.length()) {
+                        break;
+                    }
+                } if (functDict.containsKey(expr.substring(pos, scanPos))) {
                     oprPos = pos;
                     break;
                 }
-            } pos += 1;
+            } 
+            pos += 1;
         } return oprPos;
     }
 
@@ -185,38 +204,40 @@ class calculator {
         expr = expr.trim();
         String test = mask(expr);
         test = test.replaceAll("[()]", "");
-        boolean good;
+        if (test.strip() == "") {
+            expr = expr.strip().substring(1, expr.strip().length() - 1);
+        }
+        // boolean good;
         treeNode node;
-        if (test.replaceAll("\\s","").matches("^$")) {
-            expr = expr.substring(1, expr.length()-1);
-            good = true;
-        } else {good = false;}
-        System.out.println(expr);
+        // if (test.replaceAll("\\s","").matches("^$")) {
+        //     expr = expr.substring(1, expr.length()-1);
+        //     good = true;
+        // } else {
+        //     good = false;
+        // }
         ArrayList<String> oprList = new ArrayList<String>();
         ArrayList<Integer> oprPosList = new ArrayList<Integer>();
         ArrayList<String> itemList = new ArrayList<String>();
         int pos = 0;
         if (isNumber(expr)) {
             node = new treeNode(expr);
-            nodes.add(node);
             return node;
         }
         else if (isVariable(expr)) {
-            ArrayList<String> varKeys = new ArrayList(varDic.keySet());
+            ArrayList<String> varKeys = new ArrayList<String>(varDict.keySet());
             if (varKeys.contains(expr.trim())) {
-                double value = varDic.get(expr.trim());
-                System.out.println(value);
+                double value = varDict.get(expr.trim());
                 node = new treeNode(Double.toString(value));
-                nodes.add(node);
                 return node;
             } else {
                 node = new treeNode();
                 return node;
             }
-        } String opr;
+        } 
+        String opr;
         while (pos >= 0) {
             int oprPos = getNextFunctPos(mask(expr), pos);
-            int oldPos= pos;
+            int oldPos = pos;
             if (oprPos >= 0) {
                 pos = oprPos + 1;
             } else {
@@ -226,7 +247,8 @@ class calculator {
                 if (Character.isLetter(expr.charAt(oprPos))) {
                     while (Character.isLetter(expr.charAt(pos))) {
                         pos += 1;
-                    } opr = expr.substring(oprPos, pos);
+                    } 
+                    opr = expr.substring(oprPos, pos);
                 } else {
                     opr = expr.substring(oprPos, pos);
                 }
@@ -235,7 +257,6 @@ class calculator {
                     oprPos = 1;
                 }
                 String item = expr.substring(oldPos, oprPos);
-                System.out.println(item);
                 if ((isNumber(item)) || (isVariable(item))) {
                     if (functs.contains(opr)) {
                         node = new treeNode();
@@ -248,14 +269,12 @@ class calculator {
             }
         }
         int minimum = 0;
-        System.out.println(oprList);
         if (oprList.size() == 0) {
             node = new treeNode(expr.trim());
-            nodes.add(node);
             return node;
         } else {
             for (int i=1; i<oprList.size(); i++) {
-                if (functDic.get(oprList.get(i)) <= functDic.get(oprList.get(minimum))) {
+                if (functDict.get(oprList.get(i)) <= functDict.get(oprList.get(minimum))) {
                     minimum = i;
                 }
             }
@@ -264,26 +283,25 @@ class calculator {
         String minItem = itemList.get(minimum);
         int minOprPos = oprPosList.get(minimum); 
         node = new treeNode(minOpr);
-        nodes.add(node);
         if (singleFuncts.contains(minOpr)) {
             treeNode lchild = constructTree(expr.substring(0, minOprPos));
             treeNode rchild = constructTree(expr.substring(minOprPos+1));
-            node.lchild = nodes.indexOf(lchild);
-            node.rchild = nodes.indexOf(rchild);
+            node.lchild = lchild;
+            node.rchild = rchild;
         } else {
             int starting_paren = minOprPos + minOpr.length();
             String masked = mask(expr.substring(starting_paren));
             int closing_paren = starting_paren + masked.indexOf(')') + 1;
             if (!node.value.equals("mod")) {
                 treeNode lchild = constructTree(expr.substring(starting_paren, closing_paren));
-                node.lchild = nodes.indexOf(lchild);
+                node.lchild = lchild;
             } else {
                 String space = expr.substring(starting_paren, closing_paren);
                 int comma = starting_paren + space.indexOf(',');
                 treeNode lchild = constructTree(expr.substring(starting_paren+1, comma));
                 treeNode rchild = constructTree(expr.substring(comma + 1, closing_paren-1));
-                node.lchild = nodes.indexOf(lchild);
-                node.rchild = nodes.indexOf(rchild);
+                node.lchild = lchild;
+                node.rchild = rchild;
             }
         }
         return node;
@@ -292,16 +310,24 @@ class calculator {
 
     public double evaluateTree(treeNode node) {
         double returnValue;
+        if (node == null) {
+            return 0;
+        }
         try {
             returnValue = Double.parseDouble(node.value);
         } catch (NumberFormatException e) {
-            returnValue = applyFunct(node.value, evaluateTree(nodes.get(node.lchild)), evaluateTree(nodes.get(node.rchild)));
+            if (node.rchild == null) {
+                returnValue = applyFunct(node.value, evaluateTree(node.lchild));
+            } else {
+            returnValue = applyFunct(node.value, evaluateTree(node.lchild), evaluateTree(node.rchild));
+            }
         }
         return returnValue;
     }
 
     public double calc(String expr) {
-        getLines(expr);
+        ArrayList<String[]> all_lines = getLines(expr);
+
         String variable;
         for (String[] line : all_lines) {
             if (line[0] == "return") {
@@ -310,29 +336,16 @@ class calculator {
             else {
                 variable = line[0].trim();
             }
-            nodes.clear();
-            treeNode node = new treeNode("0");
-            nodes.add(node);
-            System.out.println(line[1]);
-            constructTree(line[1]);
-            for (treeNode blaargh : nodes) {
-                System.out.print(blaargh.value + ", ");
-            }
-            for (treeNode instant : nodes) {
-                System.out.println(instant.value + " " + Integer.toString(instant.lchild) + " " +Integer.toString(instant.rchild));
-            }
-            double value = evaluateTree(nodes.get(1));
-            varDic.put(variable, value);
-            System.out.println(varDic);
+            treeNode tree = constructTree(line[1]);
+            double value = evaluateTree(tree);
+            varDict.put(variable, value);
         }
-        return varDic.get("__return__");
+        return varDict.get("__return__");
     }
-
-
 
 
     public static void main(String[] args) {
-        calculator a = new calculator();
+        Calculator a = new Calculator();
         System.out.println(a.calc(args[0]));
-        }
     }
+}
